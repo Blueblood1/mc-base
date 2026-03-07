ba88711f617601d67da4237dea7768427c3fb85c
Not So Chiken <rmorton9y@yahoo.co.uk>
2026-03-07 02:49:16 +0000

    wip

--- libs/version.lua
+++ libs/version.lua
@@ -12,17 +12,33 @@ function Version.get()
     end
     
     if not fs.exists("VERSION") then
-        return "unknown"
+        return "?"
     end
     
     local file = fs.open("VERSION", "r")
+    if not file then
+        return "?"
+    end
+    
     local content = file.readAll()
     file.close()
     
+    if not content or content == "" then
+        return "?"
+    end
+    
     -- Trim whitespace and convert to number
     content = content:match("^%s*(.-)%s*$")
+    if not content or content == "" then
+        return "?"
+    end
+    
     Version.BUILD_NUMBER = tonumber(content)
-    return Version.BUILD_NUMBER or "unknown"
+    if not Version.BUILD_NUMBER then
+        return "?"
+    end
+    
+    return Version.BUILD_NUMBER
 end
 
 -- Print version banner
